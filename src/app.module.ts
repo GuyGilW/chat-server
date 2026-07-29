b13dd8da23dd96e26273c65bb2480f220ea683dc
srtr <srtr77@gmail.com>
2026-07-29 20:48:27 +0300

    chore: enable global validation pipe and CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
