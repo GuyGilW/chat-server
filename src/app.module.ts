a73edc22f4987e3a279a7aaeec612e9e864f132e
srtr <srtr77@gmail.com>
2026-07-29 11:24:08 +0300

    feat: add users model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
