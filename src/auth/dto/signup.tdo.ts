070731f77336c8a92824f8435afbeed87417acba
srtr <srtr77@gmail.com>
2026-07-29 14:31:46 +0300

    add login and signup DTOs

---
+++ src/auth/dto/signup.tdo.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class SignupDTO {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsString()
+  @MinLength(3)
+  password: string;
+}
