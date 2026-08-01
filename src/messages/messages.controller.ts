44b87ac15c7b26a5dc437ec630a0b97a91acce5b
srtr <srtr77@gmail.com>
2026-08-01 21:35:16 +0300

    feat: add messages service and controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, UseGuards, Post, Param, Get } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard/jwt-auth.guard';
+import { MessagesService } from './messages.service';
+import type { CreateMessageDto } from './dto/create-message.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor(private messagesService: MessagesService) {}
+
+  @Post('make')
+  create(
+    req: Request & { user: { userId: number } },
+    @Body() dto: CreateMessageDto,
+  ) {
+    return this.messagesService.create(
+      req.user.userId,
+      dto.chatID,
+      dto.content,
+    );
+  }
+
+  @Get('chat/:chatId')
+  findAllForChat(
+    req: Request & { user: { userId: number } },
+    @Param() chatId: string,
+  ) {
+    return this.messagesService.findAllInChat(Number(chatId), req.user.userId);
+  }
+}
