830a1f4602e5967ab57ecc96202ad337e0a062a6
srtr <srtr77@gmail.com>
2026-08-03 11:40:04 +0300

    add Routes to user endPoints

--- src/users/dto/update-password.dto.ts
+++ src/users/dto/update-password.dto.ts
@@ -3,7 +3,7 @@ import { IsString, MinLength } from 'class-validator';
 export class UpdatePasswordDto {
   @IsString()
   @MinLength(3)
-  currenPassword: string;
+  currentPassword: string;
   @IsString()
   @MinLength(3)
   newPasswrod: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUsernameDto } from './dto/update-username.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { UpdateEmailDto } from './dto/update-email.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Patch('update/username')
+  updateUsername(
+    @Req() req: Request & { user: { userId: number } },
+    @Body() dto: UpdateUsernameDto,
+  ) {
+    return this.usersService.updateUsername(req.user.userId, dto.username);
+  }
+
+  @Patch('update/password')
+  updatePassword(
+    @Req() req: Request & { user: { userId: number } },
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    return this.usersService.updatePassword(
+      req.user.userId,
+      dto.currentPassword,
+      dto.newPasswrod,
+    );
+  }
+
+  @Patch('update/email')
+  updateEmail(
+    @Req() req: Request & { user: { userId: number } },
+    @Body() dto: UpdateEmailDto,
+  ) {
+    return this.usersService.updateEmail(req.user.userId, dto.email);
+  }
+
+  @Get(':id')
+  findProfile(@Param() id: string) {
+    return this.usersService.findProfile(Number(id));
+  }
+}
